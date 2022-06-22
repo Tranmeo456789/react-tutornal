970f4dc2f659baea6fc757897dca96000754683a
Tranmeo456789 <trantrantrantrantran726@gmail.com>
2022-06-23 00:05:14 +0700

    component create

---
+++ src/component/Product.js
@@ -0,0 +1,12 @@
+
+import React, { Fragment } from 'react';
+function Product(){
+    return (
+        <React.Fragment>
+            <h1>Product</h1>
+            <button type="">create product</button>
+        </React.Fragment>      
+    );
+     
+}
+export default Product;
